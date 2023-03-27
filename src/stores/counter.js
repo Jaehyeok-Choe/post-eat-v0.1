a9fbf3b7032f7a7b4d74ba8635468b1778fa250c
Jaehyeok Choe <jaehyeokchoe@Jaehyeoks-MacBook-Air-2.local>
2023-03-27 19:11:27 +0900

    test

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,15 +1,7 @@
-// stores/counter.js
 import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', {
   state: () => {
     return { count: 1000 }
-  },
-  // could also be defined as
-  // state: () => ({ count: 0 })
-  actions: {
-    increment() {
-      this.count++
-    }
   }
 })
