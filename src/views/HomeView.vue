1ae46e71d10e8758af90fca5498adb0bd1a720ba
jaehyeokchoe <tbeben77@gmail.com>
2023-03-27 23:05:11 +0900

    erased comment

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,6 @@
 <template>
   <div class="home">
     <h1>This is 맛집 view page!</h1>
-    <!-- <div class="count">{{ counter.count }}</div>
-    <div class="buttons">
-      <v-btn
-        density="compact"
-        icon="mdi-minus"
-        size="x-large"
-        @click="counter.countDecrease"
-      ></v-btn>
-      |
-      <v-btn density="compact" icon="mdi-plus" size="x-large" @click="counter.countIncrease"></v-btn
-      ><br />
-      <hr />
-      <input type="number" v-model="counter.count" />
-    </div> -->
   </div>
 </template>
 
