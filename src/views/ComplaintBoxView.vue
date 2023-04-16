3c3c7d3a8c3667a19b944e5732ffe0c904f4d542
jaehyeokchoe <tbeben77@gmail.com>
2023-04-16 21:38:53 +0900

    working on complaintbox page

--- src/views/ComplaintBoxView.vue
+++ src/views/ComplaintBoxView.vue
@@ -1,13 +1,23 @@
 <template>
-  <div class="complaintbox">
-    <h1>Complaint Box</h1>
+  <div class="complaint-box">
+    <div class="field is-grouped">
+      <p class="control is-expanded">
+        <input class="input" type="text" placeholder="What do people complain about?" />
+      </p>
+      <p class="control">
+        <a class="button is-info"> Add </a>
+      </p>
+    </div>
   </div>
 </template>
 
 <script setup></script>
 
 <style>
-.complaintbox {
-  text-align: center;
+@import 'bulma/css/bulma.min.css';
+.complaint-box {
+  max-width: 400px;
+  padding: 20px;
+  margin: 0 auto;
 }
 </style>
